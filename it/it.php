9190b2d3260d13ef1d30f30456cb84a93f95f07b
teopz <teopz1989@gmail.com>
2018-05-17 15:56:34 +0200

    Stili

--- it/it.php
+++ it/it.php
@@ -1,1 +1,14 @@
+<?php 
+$lang = array();
 
+/* testo pulsanti dei menu */
+$lang['btn_azienda']="L'azienda";
+$lang['btn_chi']="Chi siamo";
+$lang['btn_negozio']="Il negozio";
+/* fine testo pulsanti dei menu */
+
+$lang['titolone']="Vini di qualità di Sala Monferrato";
+
+
+$lang['par_manutenzione']="Il sito è attualmente in sviluppo! Visitate la nostra pagina facebook:";
+?>
