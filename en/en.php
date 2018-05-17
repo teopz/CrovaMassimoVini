bae03a972c917668693e41cdb8fd6293b82144b5
teopz <39367407+teopz@users.noreply.github.com>
2018-05-17 11:02:58 +0200

    Create en.php

---
+++ en/en.php
@@ -0,0 +1,14 @@
+<?php 
+$lang = array();
+
+/* testo pulsanti dei menu */
+$lang['btn_azienda']="The farm";
+$lang['btn_chi']="About us";
+$lang['btn_negozio']="The shop";
+/* fine testo pulsanti dei menu */
+
+$lang['titolone']="Sala Monferrato's fine wines";
+
+
+$lang['par_manutenzione']="This site is currently under development! Please visit us on our facebook page:";
+?>
