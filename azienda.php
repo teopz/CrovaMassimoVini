36d82fc5e0e4c6da9df59c2eb94ee5853dea02cc
teopz <teopz1989@gmail.com>
2019-08-14 11:58:57 +0200

    Nuovo menu adattabile a schermi cellulari

--- azienda.php
+++ azienda.php
@@ -18,25 +18,24 @@
                 </div>
             </div>
         </div>
-        <div class="center">
-            <div class="content_menu">
-                <ul class="list" id="horiznav">
-                    <li class="list"><input type="button" class="menubuttons" onclick="document.location='index.php'" value="Home"></input></li>
-                    <li class="list"><input type="button" class="menubuttons" onclick="document.location='azienda.php'" value="<?php echo $lang['btn_azienda']?>"></input></li>
-                    <li class="list"><input type="button" class="menubuttons" onclick="document.location='eventi.php'" value="<?php echo $lang['btn_eventi']?>"></input></li>
-                    <li class="list"><input type="button" class="menubuttons" onclick="document.location='negozio.php'" value="<?php echo $lang['btn_negozio']?>"></input></li>
-                    <li class="list"><input type="button" class="menubuttons" onclick="document.location='galleria.php'" value="<?php echo $lang['btn_album']?>"></input></li>
-                    <li class="list"><input type="button" class="menubuttons" onclick="document.location='contatti.php'" value="<?php echo $lang['btn_chi']?>"></input></li>
-
-                    <li class="list"><input type="button" class="menubuttons" value="Lingua / Language"></input>
-                        <ul class="list">
-                            <li><input type="button" class="menubuttons" onclick="document.location='index.php?lang=it'" value="Italiano"></input></li>
-                            <li><input type="button" class="menubuttons" onclick="document.location='index.php?lang=en'" value="English"></input></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
+        <label for="showMenu" class="showMenu">&#9776;</label>
+        <input type="checkbox" id="showMenu" role="button"></input>
+        <ul class="ul" id="menu">
+            <li class="li"><input type="button" class="menubuttons" onclick="document.location='index.php'" value="Home"></input></li>
+            <li class="li"><input type="button" class="menubuttons" onclick="document.location='azienda.php'" value="<?php echo $lang['btn_azienda']?>"></input></li>
+            <li class="li"><input type="button" class="menubuttons" onclick="document.location='eventi.php'" value="<?php echo $lang['btn_eventi']?>"></input></li>
+            <li class="li"><input type="button" class="menubuttons" onclick="document.location='negozio.php'" value="<?php echo $lang['btn_negozio']?>"></input></li>
+            <li class="li"><input type="button" class="menubuttons" onclick="document.location='galleria.php'" value="<?php echo $lang['btn_album']?>"></input></li>
+            <li class="li"><input type="button" class="menubuttons" onclick="document.location='contatti.php'" value="<?php echo $lang['btn_chi']?>"></input></li>
+            <li class="li"><input type="button" class="menubuttons" value="Lingua / Language"><span style=\"font-size:8px\"></span></input>
+                <div class="hidden">
+                    <ul class="ul">
+                        <li class="li" style=\"border-bottom:1px solid #CCC\"><input type="button" class="menubuttons" onclick="document.location='index.php?lang=it'" value="Italiano"></input></li>
+                        <li class="li"><input type="button" class="menubuttons" onclick="document.location='index.php?lang=en'" value="English"></input></li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
         <h3 class="center"><?php echo $lang['azienda_text0']?></h3>
         <h3 class="center"><?php echo $lang['par_manutenzione']?></h3>
         <h3 class="center"><img src="images/fb.png" width="50px" height="auto"/><br /><a href="https://www.facebook.com/crovamassimovini/">Crova Massimo Vini</a></h3>
