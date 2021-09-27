73566773ff13bac418f9b2411e63f6dc34e9fb4e
teopz <39367407+teopz@users.noreply.github.com>
2021-09-27 15:00:35 +0200

    Rimossa descrizione vini non conformi

--- it/it.php
+++ it/it.php
@@ -21,18 +21,7 @@ $lang['azienda_text0']="<p class=\"pJustify\">Dal 1903 la famiglia Crova produce
     dei circa 6 ettari di vigneto che si estendono tra i comuni di Sala Monferrato e Ozzano Monferrato,
     dal 2015 facenti parte del Patrimonio Unesco per i paesaggi vitivinicoli di Langhe Roero e Monferrato. </br>
     La nostra azienda produce vari vini, tra i classici: Barbera del Monferrato DOC, Monferrato Freisa DOC,
-    Grignolino del Monferrato Casalese DOC, Monferrato Dolcetto, Monferrato Freisa e poi vini bianchi, rosati, rossi. </p>
-    <p class=\"pCenter\">Tra le specialità troviamo:</p>
-    <ul>
-    <li><p class=\"pJustify\">il “Barbera del Monferrato Superiore DOCG” affinato in botti di rovere per 11 mesi;</p></li>
-    <li><p class=\"pJustify\">il “Monferrato Rosso Notu”, il vino del nonno, un barbera di uve selezionate derivanti dal vigneto datato 1923. <br/>
-    Abbiamo voluto anche, con l’etichetta, dedicare il vino al Notu, il tris-nonno fondatore della cantina; infatti il suo volto è impresso sull’etichetta.</p></li>
-    <li><p class=\"pJustify\">“Lo sfizioso”, un vino particolare già dal nome. In un vecchio vigneto acquistato dai frati sono stati trovati vitigni di aleatico,
-    solitamente tipico dell’Elba o della Puglia. Le piantine sono state riprodotte per gemma fino ad arrivare a circa un ettaro
-    di superficie da cui si ottiene questo vino dal colore vivace, dai profumi ed aromi fruttati molto marcati, affinato in botte di rovere per 3 mesi;</p></li>
-    <li><p class=\"pJustify\">infine il particolare vino da uve rosse appassite “La coccinella”, prodotto con il metodo tradizionale; raccogliendo l’uva a fine novembre,
-    sistemandola sui graticci sulla cascina con la pazienza di togliere ogni settimana gli acini da scartare, a gennaio la torchiatura dei grappoli
-    con una fermentazione di circa 10 mesi, il nome richiama i numerosi insetti presenti nei vigneti grazie al mancato uso di diserbanti.</p></li></ul>";
+    Grignolino del Monferrato Casalese DOC, Monferrato Dolcetto, Monferrato Freisa e poi vini bianchi, rosati, rossi. </p>";
 $lang['negozio_text0']="Chiamate per conoscere la disponibilità dei nostri prodotti e le possibilità di consegna.<br /> Per il periodo di quarantena causato
 	da COVID-19 abbiamo aderito al programma Monferrato Delivery<br/> Potete trovare i nostri prodotti disponibili per consegne locali a questo indirizzo: <br/>";
 $lang['par_manutenzione']="Visitate la nostra pagina facebook:";
