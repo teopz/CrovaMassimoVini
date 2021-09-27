ded829223f3dc1fa64221a1eec8dfe8e45353d7e
teopz <39367407+teopz@users.noreply.github.com>
2021-09-27 15:01:02 +0200

    Rimossa descrizione vini non conformi

--- en/en.php
+++ en/en.php
@@ -21,17 +21,7 @@ $lang['azienda_text0']="<p class=\"pJustify\">Since 1903 Crova family produces i
     6 hectars of vineyard that spans between the villages of Sala Monferrato and Ozzano Monferrato,
     both Unesco Heritage from 2015 among the vineyard landscapes of Langhe Roero and Monferrato.<br/>
     Our farm produces various wines, from classics: Barbera del Monferrato DOC, Monferrato Freisa DOC,
-    Grignolino del Monferrato Casalese DOC, Monferrato Dolcetto, Monferrato Freisa to whites, rosè, red wines.</p>
-    <p class=\"pCenter\">Our excellences are:</p>
-    <ul><li><p class=\"pJustify\">the “Barbera del Monferrato Superiore DOCG” refined in oak barrels for 11 months;</p></li>
-    <li><p class=\"pJustify\">the “Monferrato Rosso Notu”, grandad's wine, a barbera made from premium grapes from an old vineyard from 1923. <br/>
-    We wanted, with the label, to dedicate this wine to Notu, great-great-grandad founder of our winery; for this reason his face is on the label.</p></li>
-    <li><p class=\"pJustify\">“Lo sfizioso” a particular wine you can tell from the name (delicious). In an old vineyard bought by friars was found aleatico vines,
-    usually found in Isola d'Elba or Puglia. The plants where multiplied by buds until we had enough for a cultivated surface of an hectar
-    from which we obtain this brightly colored wine, with perfumes and flavours strong and fruity, refined in oak barrels for 3 months;</p></li>
-    <li><p class=\"pJustify\">finally a particular wine from red raisins: “La coccinella”, produced with traditional methods; we harvest grapes in late November,
-    place them on grids in the barn having the patience to remove every week the bad grapes, until in January we have them pressed and then fermented
-    for 10 month. The name recalls the various bugs you can find in the vineyard thanks to lack of herbicides.</p></li></ul>";
+    Grignolino del Monferrato Casalese DOC, Monferrato Dolcetto, Monferrato Freisa to whites, rosè, red wines.</p>";
 $lang['negozio_text0']="Call us to know the availability of our products and shipping options.<br /> Due to the COVID-19 outbreak, we joined the Monferrato Delivery project.<br/>
 	You can find our products for local delivery at this link:<br/>";
 $lang['par_manutenzione']="Visit us on our facebook page:";
